e11559cf61b71a35b99bc0e161096ec20986515c
atman-33 <gpbjk0304@gmail.com>
2025-06-26 08:42:10 +0900

    feat: update package.json and vite.config.ts for deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: { alias: { '@': '/src' } }
-  // base: '/react-tailwind-ts-starter/', // Uncomment this line if deploying to GitHub Pages
+  resolve: { alias: { '@': '/src' } },
+  base: '/react-tailwind-ts-starter/', // Uncomment this line if deploying to GitHub Pages
 })
